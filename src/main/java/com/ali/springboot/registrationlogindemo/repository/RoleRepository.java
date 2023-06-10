62121d56c66a2596e14377fa1c23461ae474b34c
AAliasghar <a.asgharinia@yahoo.com>
2023-06-10 14:29:01 +0200

    Repository

---
+++ src/main/java/com/ali/springboot/registrationlogindemo/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.ali.springboot.registrationlogindemo.repository;
+
+import com.ali.springboot.registrationlogindemo.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role finaByName(String name);
+}
