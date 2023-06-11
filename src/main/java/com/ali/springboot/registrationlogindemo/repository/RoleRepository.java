88a9086d2f7666c53d91b7fe5de589da927933c6
AAliasghar <a.asgharinia@yahoo.com>
2023-06-11 12:11:17 +0200

    Thymeleaf index.html

--- src/main/java/com/ali/springboot/registrationlogindemo/repository/RoleRepository.java
+++ src/main/java/com/ali/springboot/registrationlogindemo/repository/RoleRepository.java
@@ -3,6 +3,9 @@ package com.ali.springboot.registrationlogindemo.repository;
 import com.ali.springboot.registrationlogindemo.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role finaByName(String name);
+public interface RoleRepository extends JpaRepository<Role,Long> {
+
+    Role findByName(String name);
 }
+
+
