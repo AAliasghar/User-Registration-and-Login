25c7022890c0f1f1276d89842d7ccdec98bdfd54
AAliasghar <a.asgharinia@yahoo.com>
2023-06-16 01:18:41 +0200

    Display registerd users

--- src/main/java/com/ali/springboot/registrationlogindemo/controller/AuthController.java
+++ src/main/java/com/ali/springboot/registrationlogindemo/controller/AuthController.java
@@ -4,6 +4,7 @@ import com.ali.springboot.registrationlogindemo.dto.UserDto;
 import com.ali.springboot.registrationlogindemo.entity.User;
 import com.ali.springboot.registrationlogindemo.service.UserService;
 import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -11,20 +12,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
+@AllArgsConstructor
 public class AuthController {
 
     private UserService userService;
 
+
+//    public AuthController(UserService userService) {
+//        this.userService = userService;
+//    }
+
+
     // handler method to Home Page request
     @GetMapping("/index")
-    public String home(){
+    public String home() {
         return "index";
     }
 
     // handler method to handle user registration form request
     @GetMapping("/register")
-    public String showRegistrationForm(Model model){
+    public String showRegistrationForm(Model model) {
         // create model object to store form data
         UserDto user = new UserDto();
         model.addAttribute("user", user);
@@ -36,15 +46,15 @@ public class AuthController {
     @PostMapping("/register/save")
     public String registration(@Valid @ModelAttribute("user") UserDto userDto,
                                BindingResult result,
-                               Model model){
+                               Model model) {
         User existingUser = userService.findUserByEmail(userDto.getEmail());
 
-        if(existingUser != null && existingUser.getEmail() != null && !existingUser.getEmail().isEmpty()){
+        if (existingUser != null && existingUser.getEmail() != null && !existingUser.getEmail().isEmpty()) {
             result.rejectValue("email", null,
                     "There is already an account registered with the same email");
         }
 
-        if(result.hasErrors()){
+        if (result.hasErrors()) {
             model.addAttribute("user", userDto);
             return "/register";
         }
@@ -52,4 +62,13 @@ public class AuthController {
         userService.saveUser(userDto);
         return "redirect:/register?success";
     }
+
+    // handler method to handle list of users
+    @GetMapping("/users")
+    public String users(Model model) {
+        List<UserDto> users = userService.findAllUsers();
+        model.addAttribute("users", users);
+        return "users";
+    }
+
 }
