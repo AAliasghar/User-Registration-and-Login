35840d53da449689b86db2873fb34af53be58ef6
AAliasghar <a.asgharinia@yahoo.com>
2023-06-15 08:13:45 +0200

    handle user registration form submit request

--- src/main/java/com/ali/springboot/registrationlogindemo/controller/AuthController.java
+++ src/main/java/com/ali/springboot/registrationlogindemo/controller/AuthController.java
@@ -32,5 +32,24 @@ public class AuthController {
     }
 
 
-    
+    // handler method to handle user registration form submit request
+    @PostMapping("/register/save")
+    public String registration(@Valid @ModelAttribute("user") UserDto userDto,
+                               BindingResult result,
+                               Model model){
+        User existingUser = userService.findUserByEmail(userDto.getEmail());
+
+        if(existingUser != null && existingUser.getEmail() != null && !existingUser.getEmail().isEmpty()){
+            result.rejectValue("email", null,
+                    "There is already an account registered with the same email");
+        }
+
+        if(result.hasErrors()){
+            model.addAttribute("user", userDto);
+            return "/register";
+        }
+
+        userService.saveUser(userDto);
+        return "redirect:/register?success";
+    }
 }
